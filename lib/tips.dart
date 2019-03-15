03e39bef6c5ef3d4dabc0d38063a44e57b7fd3df
kasperonline <tharejatanmay@gmail.com>
2019-03-15 17:24:22 +0530

    fixed home screen bug

--- lib/tips.dart
+++ lib/tips.dart
@@ -12,30 +12,7 @@ class _TipsState extends State<Tips> {
       appBar: AppBar(
         backgroundColor: Colors.deepOrange,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.fromLTRB(200,40.0,20.0,40.0),
-            child: Stack(
-              overflow: Overflow.visible,
-              children: <Widget>[
-                Positioned(
-                  left: 20.0,
-                  child: Container(
-                    width: 200.0,
-                    height: 400.0,
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-
-                    ),
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
+      
 
 
     );
